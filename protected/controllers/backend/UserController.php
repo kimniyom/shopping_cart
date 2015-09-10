c8e18f2195aaf9fa994626e60824a7b972171d49
kimniyom <kimniyomclub@gmail.com>
2015-09-10 16:44:29 +0700

    new commit

--- protected/controllers/backend/UserController.php
+++ protected/controllers/backend/UserController.php
@@ -7,8 +7,18 @@ class UserController extends Controller {
     public function actionUserall() {
         $use = new User();
         $data['user'] = $use->findAll();
+
+        $this->render("//backend/user/userall", $data);
+    }
+
+    public function actionDetail() {
+        $use = new User();
+        $order = new Orders();
+        $pid = $_GET['pid'];
+        $data['user'] = $use->Get_detail($pid);
+        $data['order'] = $order->get_order_user($pid);
         
-        $this->render("//user/userall",$data);
+        $this->render("//backend/user/detail", $data);
     }
 
 }
