37c2e0f8c593e8885a4b3f20137f70d54e762d8b
kimniyom <kimniyomclub@gmail.com>
2015-10-10 02:48:13 +0700

    new commit

--- protected/config/main.php
+++ protected/config/main.php
@@ -4,6 +4,7 @@
 // Yii::setPathOfAlias('local','path/to/local-folder');
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
+//Yii::setPathOfAlias('mPrint', dirname(__FILE__).'/../extensions/mPrint');
 return array(
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'Kimniyom Shopping',
@@ -29,21 +30,48 @@ return array(
     ),
     // application components
     'components' => array(
+        //...
+        'ePdf' => array(
+            'class' => 'ext.yii-pdf.EYiiPdf',
+            'params' => array(
+                'mpdf' => array(
+                    'librarySourcePath' => 'application.vendors.mpdf.*',
+                    'constants' => array(
+                        '_MPDF_TEMP_PATH' => Yii::getPathOfAlias('application.runtime'),
+                    ),
+                    'class' => 'mpdf', // the literal class filename to be loaded from the vendors folder
+                    'orientation'       => 'L', // landscape or portrait orientation
+                /* 'defaultParams'     => array( // More info: http://mpdf1.com/manual/index.php?tid=184
+                  'mode'              => '', //  This parameter specifies the mode of the new document.
+                  'format'            => 'A4', // format A4, A5, ...
+                  'default_font_size' => 0, // Sets the default document font size in points (pt)
+                  'default_font'      => '', // Sets the default font-family for the new document.
+                  'mgl'               => 15, // margin_left. Sets the page margins for the new document.
+                  'mgr'               => 15, // margin_right
+                  'mgt'               => 16, // margin_top
+                  'mgb'               => 16, // margin_bottom
+                  'mgh'               => 9, // margin_header
+                  'mgf'               => 9, // margin_footer
+                  'orientation'       => 'P', // landscape or portrait orientation
+                  ) */
+                ),
+            ),
+        ),
         'user' => array(
             // enable cookie-based authentication
             'allowAutoLogin' => true,
         ),
         // uncomment the following to enable URLs in path-format
         /*
-        'urlManager' => array(
-            'urlFormat' => 'path',
-            'showScriptName' => false,
-            'rules' => array(
-                '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-            ),
-        ),
+          'urlManager' => array(
+          'urlFormat' => 'path',
+          'showScriptName' => false,
+          'rules' => array(
+          '<controller:\w+>/<id:\d+>' => '<controller>/view',
+          '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+          '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+          ),
+          ),
          */
         'db' => array(
             'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
@@ -59,7 +87,7 @@ return array(
           ),
          * 
          */
-         
+
         'db' => array(
             'connectionString' => 'mysql:host=203.157.144.140;dbname=shopping_cart_db',
             'emulatePrepare' => true,
@@ -67,7 +95,16 @@ return array(
             'password' => 'assembler',
             'charset' => 'utf8',
         ),
-        
+   /*
+        'db' => array(
+            'connectionString' => 'mysql:host=192.0.1.18;dbname=shopping_cart_db',
+            'emulatePrepare' => true,
+            'username' => 'assembler',
+            'password' => 'assembler',
+            'charset' => 'utf8',
+        ),
+         * 
+         */
         'errorHandler' => array(
             // use 'site/error' action to display errors
             'errorAction' => 'site/error',
