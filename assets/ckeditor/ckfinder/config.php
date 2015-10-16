384397cb68a4b226100aea50dd317e04a875a91f
kimniyom <kimniyomclub@gmail.com>
2015-10-16 16:29:28 +0700

    Update2015-10-16

--- assets/ckeditor/ckfinder/config.php
+++ assets/ckeditor/ckfinder/config.php
@@ -2,8 +2,8 @@
 
 
 
-//$url = "http://localhost/shopping_cart/uploads/ck_images";
-$url = "http://www.theassembler.net/shopping_cart/uploads/ck_images";
+$url = "http://localhost/shopping_cart/uploads/ck_images";
+//$url = "http://www.theassembler.net/shopping_cart/uploads/ck_images";
 /*
  * ### CKFinder : Configuration File - Basic Instructions
  *
